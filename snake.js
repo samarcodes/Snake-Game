af3f1ce635807d2d2fca231cde66b01b2e7237fe
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2021-02-07 12:22:58 +0530

    keyboard input

--- snake.js
+++ snake.js
@@ -3,7 +3,7 @@ function init() {
 	W = canvas.width = 1000;
 	H = canvas.height = 1000;
 	pen = canvas.getContext('2d');
-	cell_size = 66;
+	cell_size = 67;
 	score = 5;
 
 	snake = {
@@ -27,13 +27,48 @@ function init() {
 			this.cells.pop();
 			var headX = this.cells[0].x;
 			var headY = this.cells[0].y;
-			var nextX = headX + 1;
-			var nextY = headY;
+			var nextX, nextY;
+
+			if(this.direction=="right") {
+				nextX = headX + 1;
+				nextY = headY;
+			}
+			else if(this.direction=="left") {
+				nextX = headX - 1;
+				nextY = headY;	
+			}
+			else if(this.direction=="down") {
+				nextX = headX;
+				nextY = headY + 1;
+			}
+			else if(this.direction=="up") {
+				nextX = headX;
+				nextY = headY - 1;
+			}
+
 			this.cells.unshift({x:nextX,y:nextY});
 		}
 	};
 
 	snake.createSnake();
+
+	//Add a Event Listener on the Document Object
+	function keyPressed(e) {
+		if(e.key=="ArrowRight") {
+			snake.direction = "right";
+		}
+		else if(e.key=="ArrowLeft") {
+			snake.direction = "left";
+		}
+		else if(e.key=="ArrowDown") {
+			snake.direction = "down";
+		}
+		else if(e.key=="ArrowUp") {
+			snake.direction = "up";
+		}
+		console.log(e.key);
+	}
+	document.addEventListener('keydown',keyPressed);
 }
 function draw() {
 	//erase prev screen
